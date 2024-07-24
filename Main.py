b57e27271bf176f8a88e092a4cb1d8ca20ad5120
Joao-08 <joao.victorva08@gmail.com>
2024-07-24 15:12:07 -0300

    Initial push 0.2

---
+++ Main.py
@@ -0,0 +1,15 @@
+# Código criado por Joao Victor Vieira Amora de Figueiredo[23.1.8019] e Henrique Angelo Duarte Alves[23.1.80]
+
+
+# Trabalho da disciplina AEDS III Ministrada pelo professor Theo Silva Lins
+#
+# Obejetivos:
+# Desenvolver a habilidade de programação de algoritmos em grafos.
+# Reforçar o aprendizado sobre os algoritmos de busca em grafos.
+# Aplicar os conhecimentos em algoritmos para resolver problemas reais.
+
+def Greetings():
+    print("Hellow World")
+
+
+Greetings()
