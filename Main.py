00924590d928f5935fb5c460d12a3a041aab2b8e
Joao-08 <joao.victorva08@gmail.com>
2024-07-28 20:54:01 -0300

    Maze Solver v0.5

--- Main.py
+++ Main.py
@@ -31,7 +31,6 @@
 # '༝' representa o caminho que o algoritmo visitou mas retornou pois não liga com o ponto final
 
 import sys
-import math
 import time
 from tkinter import Tk, filedialog
 from collections import deque
@@ -55,79 +54,83 @@ def encontrar_posicao(lab, char):
                 return (i, j)
     return None
 
-def bfs(lab, start, end):
-    rows, cols = len(lab), len(lab[0])
-    queue = deque([(start, [start])])  # Fila com a posição inicial e o caminho percorrido
-    visited = set([start])  # Conjunto de posições visitadas
-    
-    # Movimentos possíveis: direita, esquerda, para cima, para baixo
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    
-    while queue:
-        (current, path) = queue.popleft()
-        
-        if current == end:
-            return path  # Retorna o caminho se a posição final for alcançada
-        
-        for direction in directions:
-            next_row, next_col = current[0] + direction[0], current[1] + direction[1]
-            next_position = (next_row, next_col)
-            
-            if 0 <= next_row < rows and 0 <= next_col < cols and lab[next_row][next_col] != '#' and next_position not in visited:
-                queue.append((next_position, path + [next_position]))
-                visited.add(next_position)
-    
-    return None  # Retorna None se não houver caminho
+def marcar_caminho(lab, caminho, direcoes, caminho_explorado):
+    for (x, y), direcao in direcoes.items():
+        if lab[x][y] not in ('S', 'E'):
+            lab[x][y] = direcao
+    for (x, y) in caminho_explorado:
+        if lab[x][y] not in ('S', 'E', '↑', '↓', '←', '→'):
+            lab[x][y] = '༝'
 
-def dfs(lab, start, end):
-    rows, cols = len(lab), len(lab[0])
-    stack = [(start, [start])]  # Pilha com a posição inicial e o caminho percorrido
-    visited = set([start])  # Conjunto de posições visitadas
-    
-    # Movimentos possíveis: direita, esquerda, para cima, para baixo
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    
-    while stack:
-        (current, path) = stack.pop()
-        
-        if current == end:
-            return path  # Retorna o caminho se a posição final for alcançada
-        
-        for direction in directions:
-            next_row, next_col = current[0] + direction[0], current[1] + direction[1]
-            next_position = (next_row, next_col)
-            
-            if 0 <= next_row < rows and 0 <= next_col < cols and lab[next_row][next_col] != '#' and next_position not in visited:
-                stack.append((next_position, path + [next_position]))
-                visited.add(next_position)
-    
-    return None  # Retorna None se não houver caminho
+def bfs(lab, inicio, fim):
+    fila = deque([(inicio, [], {})])  # Inclui o caminho e as direções percorridas
+    visitados = set([inicio])
+    explorados = []  # Lista para armazenar todos os nós visitados
 
-def marcar_caminho(lab, caminho):
-    if caminho:
-        for row, col in caminho:
-            if lab[row][col] not in ('S', 'E'):
-                lab[row][col] = '•'
-                
+    while fila:
+        no, caminho, direcoes = fila.popleft()
+        i, j = no
+        explorados.append(no)  # Adiciona o nó atual à lista de nós visitados
+        if no == fim:
+            return caminho + [fim], explorados, direcoes
+        vizinhos = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
+        direcoes_vizinhos = {
+            (i+1, j): '↓',
+            (i-1, j): '↑',
+            (i, j+1): '→',
+            (i, j-1): '←'
+        }
+        for vizinho in vizinhos:
+            x, y = vizinho
+            if 0 <= x < len(lab) and 0 <= y < len(lab[0]) and lab[x][y] not in ('#', '█') and vizinho not in visitados:
+                fila.append((vizinho, caminho + [no], {**direcoes, **{vizinho: direcoes_vizinhos[(x, y)]}}))
+                visitados.add(vizinho)
+    return None, explorados, direcoes
+
+def dfs(lab, inicio, fim):
+    pilha = [(inicio, [], {})]  # Inclui o caminho e as direções percorridas
+    visitados = set([inicio])
+    explorados = []  # Lista para armazenar todos os nós visitados
+
+    while pilha:
+        no, caminho, direcoes = pilha.pop()
+        i, j = no
+        explorados.append(no)  # Adiciona o nó atual à lista de nós visitados
+        if no == fim:
+            return caminho + [fim], explorados, direcoes
+        vizinhos = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
+        direcoes_vizinhos = {
+            (i+1, j): '↓',
+            (i-1, j): '↑',
+            (i, j+1): '→',
+            (i, j-1): '←'
+        }
+        for vizinho in vizinhos:
+            x, y = vizinho
+            if 0 <= x < len(lab) and 0 <= y < len(lab[0]) and lab[x][y] not in ('#', '█') and vizinho not in visitados:
+                pilha.append((vizinho, caminho + [no], {**direcoes, **{vizinho: direcoes_vizinhos[(x, y)]}}))
+                visitados.add(vizinho)
+    return None, explorados, direcoes
+
 def main():
     Tk().withdraw()  # Oculta a janela principal do Tkinter
-    
+
     while True:
         print("Informe o arquivo do labirinto (Cancel para sair): ")
         arquivoLab = filedialog.askopenfilename()
-        
+
         if not arquivoLab:
             print("Fechando o programa...")
             break
-        
+
         # Crie cópias do labirinto para BFS e DFS
-        lab_bfs = ler_labirinto(arquivoLab)     
+        lab_bfs = ler_labirinto(arquivoLab)
         lab_dfs = ler_labirinto(arquivoLab)
-        
+
         if not lab_bfs or not lab_dfs:
             print("Falha ao ler o labirinto.")
             continue
-        
+
         inicio = encontrar_posicao(lab_bfs, 'S')
         fim = encontrar_posicao(lab_bfs, 'E')
 
@@ -136,29 +139,28 @@ def main():
         if not inicio or not fim:
             print("Labirinto inválido.")
             continue
-        
+
+        print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
         bfsStartTime = time.time()
         # Buscamos o caminho com a busca em largura
-        caminho_bfs = bfs(lab_bfs, inicio, fim)
-        print("Caminho Através da Busca por Largura:", caminho_bfs)
-        print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
-        print("Labirinto Através da Busca por Largura: ")
-        marcar_caminho(lab_bfs, caminho_bfs)
+        caminho_bfs, explorados_bfs, direcoes_bfs = bfs(lab_bfs, inicio, fim)
+        marcar_caminho(lab_bfs, caminho_bfs, direcoes_bfs, explorados_bfs)
+        print("Labirinto após Busca por Largura:")
         imprimir_labirinto(lab_bfs)
+        print("Nós Visitados na Busca por Largura:", explorados_bfs)
         bfsEndTime = time.time()
         print("Tempo de execução em busca de Largura: ", bfsEndTime - bfsStartTime)
-              
-        dfsStartTime = time.time()  
-        
-        # Buscamos o caminho com a busca por profundidade
-        caminho_dfs = dfs(lab_dfs, inicio, fim)
-        print("Caminho Através da Busca por Profundidade:", caminho_dfs)
+
         print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
-        print("Labirinto Através da Busca por Profundidade: ")
-        marcar_caminho(lab_dfs, caminho_dfs)
+        dfsStartTime = time.time()
+        # Buscamos o caminho com a busca por profundidade
+        caminho_dfs, explorados_dfs, direcoes_dfs = dfs(lab_dfs, inicio, fim)
+        marcar_caminho(lab_dfs, caminho_dfs, direcoes_dfs, explorados_dfs)
+        print("Labirinto após Busca por Profundidade:")
         imprimir_labirinto(lab_dfs)
-        dfsEndTime = time.time() 
+        print("Nós Visitados na Busca por Profundidade:", explorados_dfs)
+        dfsEndTime = time.time()
         print("Tempo de execução em busca de Profundidade: ", dfsEndTime - dfsStartTime)
-              
+
 if __name__ == '__main__':
     main()
